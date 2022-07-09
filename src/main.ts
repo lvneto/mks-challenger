305f0c1f655fbfd6705e23d2131b8984d4d345a8
lvneto <lauro.neto@irriga.net>
2022-07-09 02:54:41 -0300

    change default port

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const port: number = parseInt(`${process.env.PORT}`) || 3000;
+  const port: number = parseInt(`${process.env.PORT}`) || 3001;
   await app.listen(port);
 }
 bootstrap();
